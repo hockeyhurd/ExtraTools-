326826f75380e621246145ffbf5e092b417e6db6
Nick Hurd <datcodplaya@gmail.com>
2014-07-23 22:54:25 -0400

    - EntityBottler functions however many bugs are still left: 	- bottle/entity naming 	- inventory checking method
    Signed-off-by: Nick Hurd <datcodplaya@gmail.com>

--- com/hockeyhurd/item/ItemBottler.java
+++ com/hockeyhurd/item/ItemBottler.java
@@ -1,24 +1,28 @@
 package com.hockeyhurd.item;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Facing;
 import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 import com.hockeyhurd.mod.ExtraTools;
 
 public class ItemBottler extends Item {
 
-	private EntityLivingBase entityToSpawn;
+	private Entity entityToSpawn;
+	private String entityName;
 
 	public ItemBottler() {
 		super();
@@ -38,10 +42,14 @@ public class ItemBottler extends Item {
 	}
 
 	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
-		if (!(entity instanceof EntityMob || entity instanceof EntityAnimal)) return true;
-		
-		this.setUnlocalizedName("Bottled: " + entity.toString());
-		this.entityToSpawn = (EntityLivingBase) entity;
+		if (!(entity instanceof EntityCreature)) return true;
+
+		Item item = stack.getItem();
+		ItemStack thisStack = new ItemStack(item, 1, 1);
+		item.setUnlocalizedName("Bottled: " + entity.toString());
+
+		this.entityToSpawn = entity;
+		this.entityName = EntityList.getEntityString(entity);
 
 		int emptySlot = -1;
 		for (int y = 0; y < 3; y++) {
@@ -61,8 +69,8 @@ public class ItemBottler extends Item {
 
 		decreaseStackSize(stack, slotNum, player);
 
-		if (emptySlot == -1) player.worldObj.spawnEntityInWorld(new EntityItem(player.worldObj, player.posX, player.posY, player.posZ, new ItemStack(this, 1, 1)));
-		else if (emptySlot > 0) player.inventory.setInventorySlotContents(emptySlot, new ItemStack(this, 1, 1));
+		if (emptySlot == -1) player.worldObj.spawnEntityInWorld(new EntityItem(player.worldObj, player.posX, player.posY, player.posZ, thisStack));
+		else if (emptySlot > 0) player.inventory.setInventorySlotContents(emptySlot, thisStack);
 
 		player.onUpdate();
 		entity.setDead();
@@ -82,47 +90,106 @@ public class ItemBottler extends Item {
 		return stack.getItemDamage() > 0;
 	}
 
-	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
-		if (!canPlaceEntity(stack)) return stack;
+	/*
+	 * public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) { if (!canPlaceEntity(stack)) return stack;
+	 * 
+	 * else { if (this.entityToSpawn != null && !world.isRemote) { spawnCreature(world, player.posX, player.posY, player.posZ); }
+	 * 
+	 * int slotNum = checkInvForStack(new ItemStack(this, 1, 0), player); if (slotNum != -1) { player.inventory.getStackInSlot(slotNum).stackSize++; player.onUpdate(); return stack; } else if (slotNum == -1) checkInvForStack((ItemStack) null, player);
+	 * 
+	 * if (slotNum != -1) { player.inventory.setInventorySlotContents(slotNum, new ItemStack(this, 1, 0)); player.onUpdate(); return stack; } else if (slotNum == -1) world.spawnEntityInWorld(new EntityItem(world, player.posX, player.posY, player.posZ, new ItemStack(this, 1, 1)));
+	 * 
+	 * player.onUpdate(); return stack; } }
+	 */
 
+	public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer p_77659_3_) {
+		if (p_77659_2_.isRemote) {
+			return p_77659_1_;
+		}
 		else {
-			if (this.entityToSpawn != null && !world.isRemote) {
-				spawnCreature(world, player.posX, player.posY, player.posZ);
+			MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(p_77659_2_, p_77659_3_, true);
+
+			if (movingobjectposition == null) {
+				return p_77659_1_;
+			}
+			else {
+				if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
+					int i = movingobjectposition.blockX;
+					int j = movingobjectposition.blockY;
+					int k = movingobjectposition.blockZ;
+
+					if (!p_77659_2_.canMineBlock(p_77659_3_, i, j, k)) { return p_77659_1_; }
+
+					if (!p_77659_3_.canPlayerEdit(i, j, k, movingobjectposition.sideHit, p_77659_1_)) { return p_77659_1_; }
+
+					if (p_77659_2_.getBlock(i, j, k) instanceof BlockLiquid) {
+						Entity entity = spawnCreature(p_77659_2_, (double) i, (double) j, (double) k);
+
+						if (entity != null) {
+							if (entity instanceof EntityLivingBase && p_77659_1_.hasDisplayName()) {
+								((EntityLiving) entity).setCustomNameTag(p_77659_1_.getDisplayName());
+							}
+
+							if (!p_77659_3_.capabilities.isCreativeMode) {
+								--p_77659_1_.stackSize;
+							}
+						}
+					}
+				}
+
+				return p_77659_1_;
+			}
+		}
+	}
+
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World p_77648_3_, int p_77648_4_, int p_77648_5_, int p_77648_6_, int p_77648_7_, float p_77648_8_, float p_77648_9_, float p_77648_10_) {
+		if (p_77648_3_.isRemote) {
+			return true;
+		}
+		else {
+			Block block = p_77648_3_.getBlock(p_77648_4_, p_77648_5_, p_77648_6_);
+			p_77648_4_ += Facing.offsetsXForSide[p_77648_7_];
+			p_77648_5_ += Facing.offsetsYForSide[p_77648_7_];
+			p_77648_6_ += Facing.offsetsZForSide[p_77648_7_];
+			double d0 = 0.0D;
+
+			if (p_77648_7_ == 1 && block.getRenderType() == 11) {
+				d0 = 0.5D;
 			}
 
-			int slotNum = checkInvForStack(new ItemStack(this, 1, 0), player);
-			if (slotNum != -1) {
-				player.inventory.getStackInSlot(slotNum).stackSize++;
-				player.onUpdate();
-				return stack;
-			}
-			else if (slotNum == -1) checkInvForStack((ItemStack) null, player);
+			Entity entity = spawnCreature(p_77648_3_, (double) p_77648_4_ + 0.5D, (double) p_77648_5_ + d0, (double) p_77648_6_ + 0.5D);
 
-			if (slotNum != -1) {
-				player.inventory.setInventorySlotContents(slotNum, new ItemStack(this, 1, 0));
-				player.onUpdate();
-				return stack;
-			}
-			else if (slotNum == -1) world.spawnEntityInWorld(new EntityItem(world, player.posX, player.posY, player.posZ, new ItemStack(this, 1, 1)));
+			if (entity != null) {
+				if (entity instanceof EntityLivingBase && stack.hasDisplayName()) {
+					((EntityLiving) entity).setCustomNameTag(stack.getDisplayName());
+				}
 
-			player.onUpdate();
-			return stack;
+				if (!player.capabilities.isCreativeMode) {
+					--stack.stackSize;
+				}
+			}
+
+			return true;
 		}
 	}
 
 	public Entity spawnCreature(World world, double x, double y, double z) {
 		Entity entity = null;
 
-		for (int j = 0; j < 1; ++j) {
-			entity = this.entityToSpawn;
+		for (int j = 0; j < 1; j++) {
+			// entity = this.entityToSpawn;
+			entity = EntityList.createEntityByName(this.entityName, world);
 
 			if (entity != null && entity instanceof EntityLivingBase) {
 				EntityLiving entityliving = (EntityLiving) entity;
 				entity.setLocationAndAngles(x, y, z, MathHelper.wrapAngleTo180_float(world.rand.nextFloat() * 360.0F), 0.0F);
 				entityliving.rotationYawHead = entityliving.rotationYaw;
 				entityliving.renderYawOffset = entityliving.rotationYaw;
-				entityliving.onSpawnWithEgg((IEntityLivingData) null);
+				// entityliving.onSpawnWithEgg((IEntityLivingData) null);
+
+				System.out.println("Enitity: " + entity + ", Stored Entity: " + this.entityToSpawn);
 				world.spawnEntityInWorld(entity);
+				world.joinEntityInSurroundings(entity);
 				entityliving.playLivingSound();
 			}
 		}
